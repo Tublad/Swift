fec6dc93726625186ed2abc19e8b733d7ef0a866
Tublad <tublad92@gmail.com>
2019-10-18 20:15:30 +0600

    Lesson-5, Protocol Car

---
+++ Lesson-5/Lesson-5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson-5
+//
+//  Created by Евгений Шварцкопф on 18/10/2019.
+//  Copyright © 2019 Евгений Шварцкопф. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
