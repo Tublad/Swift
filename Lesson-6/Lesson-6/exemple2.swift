c93ad2dd104bc3a69a08a5923fa4a7cabfbcc573
Tublad <tublad92@gmail.com>
2019-10-24 14:31:51 +0600

    delete

---
+++ Lesson-6/Lesson-6/exemple2.swift
@@ -0,0 +1,9 @@
+//
+//  exemple2.swift
+//  Lesson-6
+//
+//  Created by Евгений Шварцкопф on 24/10/2019.
+//  Copyright © 2019 Евгений Шварцкопф. All rights reserved.
+//
+
+import Foundation
---
+++ Lesson-6/Lesson-6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson-6
+//
+//  Created by Евгений Шварцкопф on 23/10/2019.
+//  Copyright © 2019 Евгений Шварцкопф. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
