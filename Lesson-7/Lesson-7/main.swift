ce9444a326c7d935cc1ba37efbd16b04aa8c57cb
Tublad <tublad92@gmail.com>
2019-10-28 00:48:34 +0600

    add file in lesson 7

---
+++ Lesson-7/Lesson-7/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson-7
+//
+//  Created by Евгений Шварцкопф on 27/10/2019.
+//  Copyright © 2019 Евгений Шварцкопф. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
