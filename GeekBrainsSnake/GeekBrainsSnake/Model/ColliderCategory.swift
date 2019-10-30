dc71dda6d0395adafa9466031f45fa49443edc01
Tublad <tublad92@gmail.com>
2019-10-30 20:36:53 +0600

    Add project Game "Snake"

---
+++ GeekBrainsSnake/GeekBrainsSnake/Model/ColliderCategory.swift
@@ -0,0 +1,9 @@
+//
+//  ColliderCategory.swift
+//  GeekBrainsSnake
+//
+//  Created by Евгений Шварцкопф on 30/10/2019.
+//  Copyright © 2019 Евгений Шварцкопф. All rights reserved.
+//
+
+import Foundation
---
+++ GeekBrainsSnake/GeekBrainsSnake/Model/Snake.swift
@@ -0,0 +1,9 @@
+//
+//  Snake.swift
+//  GeekBrainsSnake
+//
+//  Created by Евгений Шварцкопф on 30/10/2019.
+//  Copyright © 2019 Евгений Шварцкопф. All rights reserved.
+//
+
+import Foundation
