62de8af87dda54e267be82f70032b000f07ee9a5
Tublad <tublad92@gmail.com>
2019-11-24 13:03:41 +0600

    Add code

---
+++ Social-Network/Social-Network/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Social-Network
+//
+//  Created by Евгений Шварцкопф on 23/11/2019.
+//  Copyright © 2019 Евгений Шварцкопф. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
