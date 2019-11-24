3ce7866a63aa11cddb0cf321c89cef880b1e4bfe
Tublad <tublad92@gmail.com>
2019-11-24 13:06:47 +0600

    Add code again

--- Social-Network/Social-Network/ViewController.swift
+++ Social-Network/Social-Network/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let hideAction = UITapGestureRecognizer(target: self, action: #selector(hideKeyBoard))
+        view.addGestureRecognizer(hideAction)
     }
-
-
+    
+    @objc func hideKeyBoard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func pressButton(_ sender: Any) {
+        guard let loginInput = login.text,
+            let passwordInput = password.text else {
+                return
+        }
+        guard loginInput.count > 0 && loginInput.count < 15 else {
+            return
+        }
+        guard passwordInput.count > 0 && passwordInput.count < 20 else {
+            return
+        }
+        
+        print("Hello \(loginInput) and your password \(passwordInput)")
+    }
+    
 }
 
