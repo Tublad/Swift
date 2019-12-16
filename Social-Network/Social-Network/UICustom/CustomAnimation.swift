ea28b9c62cfafd9300216937f9e61fd32e9160f0
Tublad <tublad92@gmail.com>
2019-12-16 15:15:58 +0600

    delete fail

---
+++ Social-Network/Social-Network/UICustom/CustomAnimation.swift
@@ -0,0 +1,9 @@
+//
+//  CustomAnimation.swift
+//  Social-Network
+//
+//  Created by Евгений Шварцкопф on 15.12.2019.
+//  Copyright © 2019 Евгений Шварцкопф. All rights reserved.
+//
+
+import Foundation
---
+++ Social-Network/Social-Network/animation/AnimationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AnimationViewController.swift
+//  Social-Network
+//
+//  Created by Евгений Шварцкопф on 15.12.2019.
+//  Copyright © 2019 Евгений Шварцкопф. All rights reserved.
+//
+
+import UIKit
+
+class AnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
