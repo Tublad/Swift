86915ee6fa42ec33e96635a37be5291527672ec0
Tublad <tublad92@gmail.com>
2020-01-12 00:53:16 +0600

    add view for Photo Users

--- Social-Network/Social-Network/Friends and Profile friend/ProfileFriendCell.swift
+++ Social-Network/Social-Network/Friends and Profile friend/ProfileFriendCell.swift
@@ -3,5 +3,5 @@ import UIKit
 class ProfileFriendCell: UICollectionViewCell {
     
     @IBOutlet weak var friendPhoto: UIImageView!
-    @IBOutlet weak var like: LiKeButton!
+    
 }
--- Social-Network/Social-Network/Friends and Profile friend/ProfileFriendCollectionViewController.swift
+++ Social-Network/Social-Network/Friends and Profile friend/ProfileFriendCollectionViewController.swift
@@ -5,12 +5,15 @@ import UIKit
 class ProfileFriendCollectionViewController: UICollectionViewController {
     
     var user: Friends?
-    
+    var photoUsers: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateNavigationBar()
+        
+        photoUsers = ["image1", "image2", "image3", "image4","image1", "image2", "image3", "image4","image1", "image2", "image3", "image4","image1", "image2", "image3", "image4"]
+        self.title = "Фотографии"
     }
     
     func updateNavigationBar() {
@@ -19,26 +22,36 @@ class ProfileFriendCollectionViewController: UICollectionViewController {
         navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
     }
     
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+           return 1
+       }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    @IBAction func likedButton(_ sender: Any) {
-        guard let liked = (sender as? LiKeButton) else {
-            return
-        }
-        liked.like()
+        return photoUsers.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileFriendCell", for: indexPath) as? ProfileFriendCell else {
             return UICollectionViewCell()
         }
-        guard let image = user?.imageFriend else { return UICollectionViewCell() }
-        cell.friendPhoto.image = UIImage(named: image)
+        let imageName = photoUsers[indexPath.item]
+        
+        cell.friendPhoto.image = UIImage(named: imageName)
         
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goPhoto" {
+            let cell: ProfileFriendCell = sender as! ProfileFriendCell
+            
+            let image = cell.friendPhoto.image
+            
+            let previewVC: PreviewVC = segue.destination as! PreviewVC
+            
+            previewVC.correctlyPhoto = image
+        }
+        
     }
     
 }
